9be5b60da29fe4701d44c4f12b342642b5b83dcc
sam_nguyen <sam.nv@neo-lab.vn>
2017-10-31 15:48:52 +0700

    done

--- Inventory/app/src/main/java/com/polahub/pos/inventory/InventoryActivity.java
+++ Inventory/app/src/main/java/com/polahub/pos/inventory/InventoryActivity.java
@@ -1,6 +1,11 @@
 package com.polahub.pos.inventory;
 
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +25,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,6 +53,15 @@ public class InventoryActivity extends AppCompatActivity implements NavigationVi
 
     @BindView(R.id.navigation)
     BottomNavigationView mBottomNavigationView;
+
+    private interface WindowCompat {
+        void setStatusBarColor(int color);
+    }
+
+    private int mPrimaryColor;
+    private int mAccentColor;
+
+    private List<Item> mItems;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -81,10 +101,14 @@ public class InventoryActivity extends AppCompatActivity implements NavigationVi
         toggle.syncState();
             mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mNavigationView.setNavigationItemSelectedListener(this);
-
+            mBottomNavigationView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
         mInventoryPagerAdapter = new InventoryPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mInventoryPagerAdapter);
+            final WindowCompat windowCompat = windowCompat(this);
             mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+                private ValueAnimator mPrimaryAnimator;
+                private ValueAnimator mAccentAnimator;
                 @Override
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -92,13 +116,53 @@ public class InventoryActivity extends AppCompatActivity implements NavigationVi
 
                 @Override
                 public void onPageSelected(int position) {
+
+                    if (mPrimaryAnimator != null
+                            && mPrimaryAnimator.isRunning()) {
+                        mPrimaryAnimator.cancel();
+                    }
+
+                    if (mAccentAnimator != null
+                            && mAccentAnimator.isRunning()) {
+                        mAccentAnimator.cancel();
+                    }
+
+                    // right, if
+                    final Item item = mItems.get(position);
+
+                    final CCFAnimator primary = CCFAnimator.rgb(mPrimaryColor, item.primaryColor);
+                    mPrimaryAnimator = primary.asValueAnimator(new CCFAnimator.OnNewColorListener() {
+                        @Override
+                        public void onNewColor(@ColorInt int color) {
+                            windowCompat.setStatusBarColor(color);
+                        }
+                    });
+                    mPrimaryAnimator.setDuration(250L);
+                    mPrimaryAnimator.start();
+
+                    // we need to evaluate colors (in case we are currently in scrolling transaction
+                    final CCFAnimator accent = CCFAnimator.rgb(
+                            accentColor(mAccentColor, mPrimaryColor),
+                            accentColor(item.accentColor, item.primaryColor)
+                    );
+                    mAccentAnimator = accent.asValueAnimator(new CCFAnimator.OnNewColorListener() {
+                        @Override
+                        public void onNewColor(@ColorInt int color) {
+                            mBottomNavigationView.setBackgroundColor(color);
+                            mToolbar.setBackgroundColor(color);
+                        }
+                    });
+                    mAccentAnimator.setDuration(250L);
+                    mAccentAnimator.start();
+
+                    mPrimaryColor = item.primaryColor;
+                    mAccentColor = item.accentColor;
+
                     switch (position) {
                         case InventoryType.HOME:
-                            Log.e("HOME", "HOME");
                             mBottomNavigationView.setSelectedItemId(R.id.navigation_home);
                             break;
                         case InventoryType.DASHBOARD:
-                            Log.e("DASHBOARD", "DASHBOARD");
                             mBottomNavigationView.setSelectedItemId(R.id.navigation_dashboard);
                             break;
                         case InventoryType.NOTIFICATIONS:
@@ -118,6 +182,32 @@ public class InventoryActivity extends AppCompatActivity implements NavigationVi
 
                 }
             });
+
+            mItems = new ArrayList<>();
+
+            mItems.add(new Item(
+                    ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary),
+                    ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary),
+                    "First Teal"
+            ));
+
+            mItems.add(new Item(
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_red_500),
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_red_300),
+                    "Second Red"
+            ));
+
+            mItems.add(new Item(
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_purple_500),
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_purple_300),
+                    "Third Purple"
+            ));
+
+            mItems.add(new Item(
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_blue_500),
+                    ContextCompat.getColor(getApplicationContext(), R.color.md_blue_300),
+                    "Forth Blue"
+            ));
 
     }
 
@@ -217,5 +307,62 @@ public class InventoryActivity extends AppCompatActivity implements NavigationVi
         public static final int DASHBOARD = 1;
         public static final int NOTIFICATIONS = 2;
         public static final int SETTINGS = 3;
+    }
+
+    private interface ViewProvider {
+        View provide(ViewGroup parent);
+    }
+
+    private static class Item {
+
+        final int primaryColor;
+        final int accentColor;
+        final String title;
+
+        private Item(int primaryColor, int accentColor, String title) {
+            this.primaryColor = primaryColor;
+            this.accentColor = accentColor;
+            this.title = title;
+        }
+    }
+
+    private static WindowCompat windowCompat(Activity activity) {
+        final WindowCompat compat;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            compat = new WindowCompat21(activity.getWindow());
+        } else {
+            compat = new WindowCompatImpl();
+        }
+        return compat;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static class WindowCompat21 implements WindowCompat {
+
+        private final Window mWindow;
+
+        private WindowCompat21(Window window) {
+            mWindow = window;
+        }
+
+        @Override
+        public void setStatusBarColor(int color) {
+            mWindow.setStatusBarColor(color);
+        }
+    }
+
+    private static class WindowCompatImpl implements WindowCompat {
+
+        @Override
+        public void setStatusBarColor(int color) {
+            // no op
+        }
+    }
+
+    private static int accentColor(int accentColor, int primaryColor) {
+        int out;
+         CCFAnimator animator = CCFAnimator.rgb(accentColor, primaryColor);
+        out = animator.getColor(1);
+        return out;
     }
 }
